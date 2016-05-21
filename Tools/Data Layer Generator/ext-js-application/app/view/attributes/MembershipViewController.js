2c969c6cdbf666ee09bc5ec42c9b53de1dfc1412
Аббас Гусенов <gusenov@live.ru>
2016-05-21 19:23:13 +0600

    Вид принадлежности атрибутов к сущностям

---
+++ Tools/Data Layer Generator/ext-js-application/app/view/attributes/MembershipViewController.js
@@ -0,0 +1,10 @@
+/*
+Данный файл был создан после команды:
+sencha generate view attributes.MembershipView
+*/
+
+Ext.define('Dalagen.view.attributes.MembershipViewController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.attributes-membershipview'
+    
+});
---
+++ Tools/Data Layer Generator/ext-js-application/app/view/attributes/MembershipViewModel.js
@@ -0,0 +1,13 @@
+/*
+Данный файл был создан после команды:
+sencha generate view attributes.MembershipView
+*/
+
+Ext.define('Dalagen.view.attributes.MembershipViewModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.attributes-membershipview',
+    data: {
+        name: 'Dalagen'
+    }
+
+});
