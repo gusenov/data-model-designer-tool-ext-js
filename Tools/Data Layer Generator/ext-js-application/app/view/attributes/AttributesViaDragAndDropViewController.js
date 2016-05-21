6e61f86edc2d4532a816b0834787c7dbb3d6d556
Аббас Гусенов <gusenov@live.ru>
2016-05-21 19:21:55 +0600

    Вид атрибутов через drag'n'drop способ

---
+++ Tools/Data Layer Generator/ext-js-application/app/view/attributes/AttributesViaDragAndDropViewController.js
@@ -0,0 +1,10 @@
+/*
+Данный файл был создан после команды:
+sencha generate view attributes.AttributesViaDragAndDropView
+*/
+
+Ext.define('Dalagen.view.attributes.AttributesViaDragAndDropViewController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.attributes-attributesviadraganddropview'
+    
+});
---
+++ Tools/Data Layer Generator/ext-js-application/app/view/attributes/AttributesViaDragAndDropViewModel.js
@@ -0,0 +1,13 @@
+/*
+Данный файл был создан после команды:
+sencha generate view attributes.AttributesViaDragAndDropView
+*/
+
+Ext.define('Dalagen.view.attributes.AttributesViaDragAndDropViewModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.attributes-attributesviadraganddropview',
+    data: {
+        name: 'Dalagen'
+    }
+
+});
